fe74cdb4a5b7e35969fbd79025affdced367c4be
Kevin-Pember <Kevinkzp@gmail.com>
2024-08-29 19:15:01 -0700

    Lab 1

---
+++ Lab1.py
@@ -0,0 +1,10 @@
+#My First Python Program.
+print("The itsy bitsy spider climbed up the waterspout")
+print("Down the rain")
+print("and washed the spider out")
+
+print(" _  __ _____ _     _  _        ____  _____ _      ____  _____ ____ ")
+print("/ |/ //  __// \ |\/ \/ \  /|  /  __\/  __// \__/|/  _ \/  __//  __\ ")
+print("|   / |  \  | | //| || |\ ||  |  \/||  \  | |\/||| | //|  \  |  \/|")
+print("|   \ |  /_ | \// | || | \||  |  __/|  /_ | |  ||| |_\\\\|  /_ |    /")
+print("\_|\_\\\\____\\\\__/  \_/\_/  \|  \_/   \____\\\\_/  \|\____/\____\\\\_/\_\ ")
